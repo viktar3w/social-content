2c125205edb4d183d9e791a5ef59582ebc49a14d
ViktarStar <v.starovoitou@belvg.com>
2024-07-29 10:47:49 +0200

    fix message at UpgradeCard component

--- components/cards/UpgradeCard.tsx
+++ components/cards/UpgradeCard.tsx
@@ -39,7 +39,7 @@ const UpgradeCard = () => {
       <CardContent>
         <div>
           <p className="flex my-2 gap-2">
-            <Check /> 100,000 words/purchase
+            <Check /> 10,000 words/purchase
           </p>
           <p className="flex my-2 gap-2">
             <Check /> All Template Access
