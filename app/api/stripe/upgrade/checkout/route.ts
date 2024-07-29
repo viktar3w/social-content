92863158002990520a383792dc2adb64e330b6f5
ViktarStar <v.starovoitou@belvg.com>
2024-07-29 16:06:35 +0200

    analyze problem with stripe webhook

--- app/api/stripe/upgrade/checkout/route.ts
+++ app/api/stripe/upgrade/checkout/route.ts
@@ -1,30 +1,16 @@
 import { auth, currentUser } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
-import Stripe from "stripe";
 import { db } from "@/lib/db";
 import { stripe } from "@/lib/stripe";
 import { DEFAULT_LENGTH_AI } from "@/consts/settings";
 
-export const POST = async (req: Request) => {
+export const POST = async () => {
   try {
     const { userId } = auth();
     const user = await currentUser();
     if (!userId || !user || !user?.emailAddresses?.[0].emailAddress) {
       return new NextResponse("User not authenticated", { status: 401 });
     }
-    const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [
-      {
-        quantity: 1,
-        price_data: {
-          currency: "USD",
-          product_data: {
-            name: "10,000 AI Credit",
-            description: "all $10 worth of credit",
-          },
-          unit_amount: 1000,
-        },
-      },
-    ];
     await db.purchase.create({
       data: {
         userId,
@@ -43,24 +29,34 @@ export const POST = async (req: Request) => {
       const customer = await stripe.customers.create({
         email: user.emailAddresses[0].emailAddress,
       });
-      let stripeCustomer = await db.stripeCustomer.create({
+      await db.stripeCustomer.create({
         data: {
           userId,
           stripeCustomerId: customer.id,
         },
       });
     }
-    const session = await stripe.checkout.sessions.create({
-      customer: stripeCustomer?.stripeCustomerId,
-      line_items,
-      mode: "payment",
+
+    const product = await stripe.products.create({
+      name: "10,000 AI Credit",
+      description: "all $10 worth of credit",
+      default_price_data: {
+        currency: "USD",
+        unit_amount: 1000,
+      },
+    });
+
+    const stripeSession = await stripe.checkout.sessions.create({
       success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/dashboard`,
       cancel_url: process.env.NEXT_PUBLIC_SERVER_URL,
+      payment_method_types: ["card"],
+      mode: "payment",
       metadata: {
         userId: userId,
       },
+      line_items: [{ price: product.default_price as string, quantity: 1 }],
     });
-    return NextResponse.json({ url: session.url });
+    return NextResponse.json({ url: stripeSession.url });
   } catch (err: any) {
     console.log("[ERROR] ", err);
     return new NextResponse("User not authenticated", { status: 500 });
